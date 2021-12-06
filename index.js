fe5f6ef775f1d31e7924f1ba3b76f315c8d5b246
wesleyc <wesley_chang@hotmail.com>
2021-12-05 21:04:35 -0700

    Add documentation blocks
    Adding documentation blocks to outline what each function does to provide better context

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+/**
+ * Compare 2 semantic versioning strings and returns the one with
+ * higher precedence.
+ * @param  {String} v1 The first semantic versioning string
+ * @param  {String} v2 The second semantic versioning string
+ * @return {String}    The semantic versioning string with higher precedence
+ */
 function compareVersions(v1, v2) {
 	if (!isValidVersionType(v1) || !isValidVersionType(v2)) {
 		return 'Invalid version type';
@@ -34,10 +41,19 @@ function compareVersions(v1, v2) {
 				case 0:
 					return v1;
 			}
-			
 	}
 }
 
+/**
+ * Compare the core version portion of the semantic versioning strings and return
+ * the result.
+ * @param  {String} coreVersion1 The first semantic versioning core version string
+ * @param  {String} coreVersion2 The second semantic versioning core version string
+ * @return {Number}              The comparison result (1 meaning the first string has
+ *                               the higher precedence, -1 meaning the second string has
+ *                               has the higher precedence, and 0 meaning both strings 
+ *                               have equal precedence.
+ */
 function compareCoreVersions(coreVersion1, coreVersion2) {
 	const v1Parts = coreVersion1.split('.');
 	const v2Parts = coreVersion2.split('.');
@@ -57,6 +73,16 @@ function compareCoreVersions(coreVersion1, coreVersion2) {
 	return 0;
 }
 
+/**
+ * Compare the pre-release portion of the semantic versioning strings and return
+ * the result.
+ * @param  {String} preReleaseVersion1 The first semantic versioning pre-release version string
+ * @param  {String} preReleaseVersion1 The second semantic versioning pre-release version string
+ * @return {Number}                    The comparison result (1 meaning the first string has
+ *                                     the higher precedence, -1 meaning the second string has
+ *                                     has the higher precedence, and 0 meaning both strings 
+ *                                     have equal precedence.
+ */
 function comparePreReleaseVersion(preReleaseVersion1, preReleaseVersion2) {
 	if (preReleaseVersion1 === '' && preReleaseVersion2 === '') {
 		return 1;
@@ -86,12 +112,25 @@ function comparePreReleaseVersion(preReleaseVersion1, preReleaseVersion2) {
 	}
 }
 
+/**
+ * Validate the semantic versioning string provided is a valid format by using the regular
+ * expression supplied by https://semver.org/
+ * @param  {String}  version The semantic versioning string to validate against
+ * @return {Boolean}         TRUE if the supplied semantic versioning string has a valid format
+ *                           FALSE otherwise     
+ */
 function isValidVersionFormat(version) {
 	const regex = /^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/;
 				
 	return regex.test(version);
 }
 
+/**
+ * Validate the semantic versioning string provided is a valid type
+ * @param  {String}  version The semantic versioning string to validate against
+ * @return {Boolean}         TRUE if the supplied semantic versioning string has a valid type
+ *                           FALSE otherwise     
+ */
 function isValidVersionType(version) {		
 	return typeof version === 'string';
 }
